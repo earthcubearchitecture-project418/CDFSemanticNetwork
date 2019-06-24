1c059d5525d8a072e3bb2e0a78a0e2a2617e9ced
Douglas Fils <drfils@gmail.com>
2019-06-24 17:29:49 -0500

    file name fixes and added a loader script

---
+++ scripts/load2Jena.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# A wrapper script for loading RDF into Jena 
+# usage:  load2jena.sh directory targetDataBase  
+# example: ./load2jena.sh ../runs/CDFSemNetD1 p419  
+pushd $1
+
+files=$( ls -1  *.nq | grep -v shacl | grep -v prov   )
+counter=0
+for i in $files ; do
+      echo "-------------start-------------"
+      echo Next: $i
+      # rapper -e -c -i nquads $i 
+      curl -X POST --header "Content-Type:application/n-quads" -d @$i http://localhost:3030/$2/data?graph=$3
+      echo "-------------done--------------"
+done
+
+popd
